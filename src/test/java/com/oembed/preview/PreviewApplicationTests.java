ec3d215c8beab2f2c794d70ef17b713f3ff2da35
doongjun <doongjun.kim@gmail.com>
2022-08-12 13:35:34 +0900

    init setting

---
+++ src/test/java/com/oembed/preview/PreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oembed.preview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

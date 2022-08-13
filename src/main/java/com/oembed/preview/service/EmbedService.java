568e1ee6e3289817cc260b2c3d2f55edc156ce01
doongjun <doongjun@kakao.com>
2022-08-13 10:34:06 +0900

    Update: providers endpoint 데이터 정제

--- src/main/java/com/oembed/preview/service/EmbedService.java
+++ src/main/java/com/oembed/preview/service/EmbedService.java
@@ -1,6 +1,7 @@
 package com.oembed.preview.service;
 
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
@@ -8,9 +9,21 @@ import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class EmbedService {
+
+    public Map<String, Object> getOembedResponse(String url) throws ParseException, URISyntaxException {
+        String urlHost = getUrlHost(url);
+        JSONArray providers = getProviders();
+        String endpointUrl = getEndpointUrl(urlHost, providers);
+        String requestUrl = createRequestUrl(url, endpointUrl);
+
+        Map<String, Object> map = new HashMap<>();
+        return map;
+    }
 
     /**
      * url의 host의 값을 가져온다.
@@ -18,7 +31,7 @@ public class EmbedService {
      * @return
      * @throws URISyntaxException
      */
-    public String getUrlHost(String paramUrl) throws URISyntaxException {
+    private String getUrlHost(String paramUrl) throws URISyntaxException {
         URI uri = new URI(paramUrl);
         String host = uri.getHost();
         if (host.startsWith("www.")) {
@@ -32,12 +45,41 @@ public class EmbedService {
      * @return
      * @throws ParseException
      */
-    private static JSONArray getProviders() throws ParseException {
+    private JSONArray getProviders() throws ParseException {
         String providersUrl = "https://oembed.com/providers.json";
 
         RestTemplate restTemplate = new RestTemplate();
         String jsonStr = restTemplate.getForObject(providersUrl, String.class);
         return (JSONArray) new JSONParser().parse(jsonStr);
+    }
+
+    /**
+     * providers의 urlHost endpoint를 가져온다..
+     * @param urlHost
+     * @param providers
+     * @return
+     */
+    private String getEndpointUrl(String urlHost, JSONArray providers) {
+        String endpointUrl = "";
+        for(Object o : providers) {
+            JSONObject jsonObject = (JSONObject) o;
+            String providerUrl = jsonObject.get("provider_url").toString();
+
+            if(providerUrl.contains(urlHost)) {
+               JSONArray endpoints = (JSONArray) jsonObject.get("endpoints");
+                JSONObject endpoint = (JSONObject) endpoints.get(0);
+                endpointUrl = endpoint.get("url").toString();
+                break;
+            }
+        }
+        return endpointUrl;
+    }
+
+    private String createRequestUrl(String url, String endpointUrl) {
+        if(endpointUrl.endsWith("{format}")) {
+            endpointUrl = endpointUrl.substring(0, endpointUrl.lastIndexOf("{")) + "json";
+        }
+        return endpointUrl + "?url=" + url;
     }
 
 }

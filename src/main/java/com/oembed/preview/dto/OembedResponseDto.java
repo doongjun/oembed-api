9039defdf0cbe8fca82b2a5de7c90c2096906148
doongjun <doongjun@kakao.com>
2022-08-14 13:21:55 +0900

    Update: response 항목 추가

--- src/main/java/com/oembed/preview/dto/OembedResponseDto.java
+++ src/main/java/com/oembed/preview/dto/OembedResponseDto.java
@@ -12,11 +12,18 @@ public class OembedResponseDto {
     String type;
     int height;
     int width;
+    int duration;
+    private String description;
     String version;
     String provider_name;
     String provider_url;
     int thumbnail_height;
     int thumbnail_width;
     String thumbnail_url;
+    String thumbnail_url_with_play_button;
     String html;
+    String uri;
+    String upload_date;
+    Long video_id;
+    String is_plus;
 }
